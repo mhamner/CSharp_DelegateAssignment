aeb267b223e013cc2fa682588f65ea69f6f312d9
mhamner <37221769+mhamner@users.noreply.github.com>
2021-06-08 15:31:39 -0400

    Add files via upload

---
+++ CSharp_DelegateAssignment/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_DelegateAssignment
+{
+    delegate void MathDelegate(int numberA, int numberB);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter Number A:");
+            int numA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter Number B:");
+            int numB = Convert.ToInt32(Console.ReadLine());
+
+            //Create a new instance of our delegate
+            MathDelegate mathDelegate;
+            
+            if(numA > numB)
+            {
+                mathDelegate = SubtractAB;
+            }
+            else
+            {
+                mathDelegate = SubtractBA;
+            }
+
+            mathDelegate += MultiplyAB;
+
+            if(numA > 0 && numB > 0)
+            {
+                mathDelegate += DivideAB;
+                mathDelegate += DivideBA;
+            }
+
+            //Execute all the chained-together delegates
+            mathDelegate(numA, numB);
+
+            Console.WriteLine("Press any key to end.");
+            Console.ReadKey();
+        }
+
+        static void SubtractAB(int numberA, int numberB)
+        {
+            Console.WriteLine($"{numberA} - {numberB} = {numberA - numberB}.");
+        }
+
+        static void SubtractBA(int numberA, int numberB)
+        {
+            Console.WriteLine($"{numberB} - {numberA} = {numberB - numberA}.");
+        }
+
+        static void MultiplyAB(int numberA, int numberB)
+        {
+            Console.WriteLine($"{numberA} * {numberB} = {numberA * numberB}.");
+        }
+
+        static void DivideAB(int numberA, int numberB)
+        {
+            Console.WriteLine($"{numberA} / {numberB} = {numberA / numberB}.");
+        }
+
+        static void DivideBA(int numberA, int numberB)
+        {
+            Console.WriteLine($"{numberB} / {numberA} = {numberB / numberA}.");
+        }
+    }
+}
